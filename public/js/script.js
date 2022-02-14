9b5d594f9cb72fa2cfde2214f23b0214b387c42b
Olayinka Oginni <olayinkaoginni2016@gmail.com>
2022-02-14 11:07:58 +0100

    test commit

---
+++ public/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("Drillie JS imported successfully!");
+  },
+  false
+);
---
+++ utils/consts.js
@@ -0,0 +1,3 @@
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/Drillie";
+
+module.exports = MONGO_URI;
