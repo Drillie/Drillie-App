bfbd64e3edb8e0050dcec137fb46eb1fa2ae604b
Jonas Darley <jonasdarley@live.com>
2022-02-15 16:12:59 +0100

    added view details and edit page for projects

--- public/js/script.js
+++ public/js/script.js
@@ -6,10 +6,16 @@ document.addEventListener(
   false
 );
 
+// Add Project Button
+
 document.getElementById('addProjectButton').addEventListener('click', function() {
   document.querySelector('.bg-modal').style.display = 'flex'
 })
 
+// Close the add-project-modal Button
+
 document.querySelector('.close').addEventListener('click', function() {
   document.querySelector('.bg-modal').style.display = 'none'
 })
+
+
