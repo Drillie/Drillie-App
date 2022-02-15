9a6939edad3b0aa0966cf8fe4416a0ade0e298db
Lorelai <nadja.rathenow@rub.de>
2022-02-15 12:21:04 +0100

    E-Mail inserted in Signup/Login

--- models/User.model.js
+++ models/User.model.js
@@ -3,9 +3,7 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema(
   {
     username: {
-      type: String,
-      unique: true,
-      required: true
+      type: String
     },
     email: {
       type: String,
