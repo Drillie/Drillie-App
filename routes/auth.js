224c29651b331075fc5cd08622875ea851babde4
Olayinka Oginni <olayinkaoginni2016@gmail.com>
2022-02-16 14:13:02 +0100

    tools added to the user model

--- routes/auth.js
+++ routes/auth.js
@@ -1,36 +1,39 @@
-const router = require("express").Router();
+const router = require('express').Router()
+const Tool = require('../models/Tool.model')
 
 // ℹ️ Handles password encryption
-const bcrypt = require("bcrypt");
-const mongoose = require("mongoose");
+const bcrypt = require('bcrypt')
+const mongoose = require('mongoose')
 
 // How many rounds should bcrypt run the salt (default [10 - 12 rounds])
-const saltRounds = 10;
+const saltRounds = 10
 
 // Require the User model in order to interact with the database
-const User = require("../models/User.model");
+const User = require('../models/User.model')
 
 // Require necessary (isLoggedOut and isLiggedIn) middleware in order to control access to specific routes
-const isLoggedOut = require("../middleware/isLoggedOut");
-const isLoggedIn = require("../middleware/isLoggedIn");
+const isLoggedOut = require('../middleware/isLoggedOut')
+const isLoggedIn = require('../middleware/isLoggedIn')
 
-router.get("/signup", isLoggedOut, (req, res) => {
-  res.render("auth/signup");
-});
+router.get('/signup', isLoggedOut, (req, res) => {
+  Tool.find().then((tools) => {
+    res.render('auth/signup', { tools: tools })
+  })
+})
 
-router.post("/signup", isLoggedOut, (req, res) => {
-  const { username, email, password } = req.body;
+router.post('/signup', isLoggedOut, (req, res) => {
+  const { username, email, password, toolsAvailable } = req.body
 
   if (!email) {
     return res
       .status(400)
-      .render("auth/signup", { errorMessage: "Please provide your E-Mail." });
+      .render('auth/signup', { errorMessage: 'Please provide your E-Mail.' })
   }
 
-  if (password.length < 8) {
-    return res.status(400).render("auth/signup", {
-      errorMessage: "Your password needs to be at least 8 characters long.",
-    });
+  if (password.length < 3) {
+    return res.status(400).render('auth/signup', {
+      errorMessage: 'Your password needs to be at least 8 characters long.',
+    })
   }
 
   //   ! This use case is using a regular expression to control for special characters and min length
@@ -51,7 +54,7 @@ router.post("/signup", isLoggedOut, (req, res) => {
     if (found) {
       return res
         .status(400)
-        .render("auth/signup", { errorMessage: "E-Mail already taken." });
+        .render('auth/signup', { errorMessage: 'E-Mail already taken.' })
     }
 
     // if user is not found, create a new user - start with hashing the password
@@ -64,51 +67,52 @@ router.post("/signup", isLoggedOut, (req, res) => {
           username,
           email,
           password: hashedPassword,
-        });
+          toolsAvailable,
+        })
       })
       .then((user) => {
         // Bind the user to the session object
-        req.session.user = user;
-        res.redirect("/");
+        req.session.user = user
+        res.redirect('/')
       })
       .catch((error) => {
         if (error instanceof mongoose.Error.ValidationError) {
           return res
             .status(400)
-            .render("auth/signup", { errorMessage: error.message });
+            .render('auth/signup', { errorMessage: error.message })
         }
         if (error.code === 11000) {
-          return res.status(400).render("auth/signup", {
+          return res.status(400).render('auth/signup', {
             errorMessage:
-              "Name needs to be unique. The Username you chose is already in use."
-          });
+              'Name needs to be unique. The Username you chose is already in use.',
+          })
         }
         return res
           .status(500)
-          .render("auth/signup", { errorMessage: error.message });
-      });
-  });
-});
+          .render('auth/signup', { errorMessage: error.message })
+      })
+  })
+})
 
-router.get("/login", isLoggedOut, (req, res) => {
-  res.render("auth/login");
-});
+router.get('/login', isLoggedOut, (req, res) => {
+  res.render('auth/login')
+})
 
-router.post("/login", isLoggedOut, (req, res, next) => {
-  const { email, password } = req.body;
+router.post('/login', isLoggedOut, (req, res, next) => {
+  const { email, password } = req.body
 
   if (!email) {
     return res
       .status(400)
-      .render("auth/login", { errorMessage: "Please provide your E-Mail." });
+      .render('auth/login', { errorMessage: 'Please provide your E-Mail.' })
   }
 
   // Here we use the same logic as above
   // - either length based parameters or we check the strength of a password
-  if (password.length < 8) {
-    return res.status(400).render("auth/login", {
-      errorMessage: "Your password needs to be at least 8 characters long.",
-    });
+  if (password.length < 3) {
+    return res.status(400).render('auth/login', {
+      errorMessage: 'Your password needs to be at least 8 characters long.',
+    })
   }
 
   // Search the database for a user with the username submitted in the form
@@ -118,7 +122,7 @@ router.post("/login", isLoggedOut, (req, res, next) => {
       if (!user) {
         return res
           .status(400)
-          .render("auth/login", { errorMessage: "Wrong credentials." });
+          .render('auth/login', { errorMessage: 'Wrong credentials.' })
       }
 
       // If user is found based on the username, check if the in putted password matches the one saved in the database
@@ -126,31 +130,25 @@ router.post("/login", isLoggedOut, (req, res, next) => {
         if (!isSamePassword) {
           return res
             .status(400)
-            .render("auth/login", { errorMessage: "Wrong credentials." });
+            .render('auth/login', { errorMessage: 'Wrong credentials.' })
         }
-        req.session.user = user;
+        req.session.user = user
         // req.session.user = user._id; // ! better and safer but in this case we saving the entire user object
-        return res.redirect("/");
-      });
+        return res.redirect('/')
+      })
     })
 
     .catch((err) => {
       // in this case we are sending the error handling to the error handling middleware that is defined in the error handling file
       // you can just as easily run the res.status that is commented out below
-      next(err);
+      next(err)
       // return res.status(500).render("login", { errorMessage: err.message });
-    });
-});
+    })
+})
 
-router.get("/logout", isLoggedIn, (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      return res
-        .status(500)
-        .render("auth/logout", { errorMessage: err.message });
-    }
-    res.redirect("/login");
-  });
-});
-
-module.exports = router;
+//logout
+router.get('/logout', (req, res, next) => {
+  req.session.destroy()
+  res.redirect('/login')
+})
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,60 @@
-const router = require("express").Router();
-const User = require("../models/User.model");
-const Tool = require("../models/Tool.model");
-const Project = require("../models/Project.model");
+const router = require('express').Router()
+const User = require('../models/User.model')
+const Tool = require('../models/Tool.model')
+const Project = require('../models/Project.model')
 
-const fileUploader = require('../config/cloudinary.config');
-
-/* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
-});
+const fileUploader = require('../config/cloudinary.config')
 
 /* GET home page */
-router.get("/profile", (req, res, next) => {
-  res.render("profile");
-});
+router.get('/', (req, res, next) => {
+  res.render('index')
+})
 
+/* GET home page */
+router.get('/profile', (req, res, next) => {
+  res.render('profile')
+})
+// Show Profile
+router.get('/profile', (req, res, next) => {
+  const id = req.session.user._id
+  User.findById(id)
+    .populate('toolsAvailable')
+    .then((currentUser) => {
+      res.render('profile/profile', { userDetails: currentUser })
+    })
+    .catch((err) => next(err))
+})
+
+// Edit Profile
+router.get('/profile/edit', (req, res, next) => {
+  const id = req.session.user._id
+  User.findById(id, function (req1, currentUser) {
+    Tool.find({}, function (req2, toolsFromDb) {
+      res.render('profile/edit', {
+        userToBeEdited: currentUser,
+        tools: toolsFromDb,
+      })
+    })
+  })
+})
 
 // Render two collections at the same time, in order to get the tools and also show all the current projects
 
-router.get("/post-project", function(req, res) {
-  Tool.find({}, function(err, tools) {
-       if(err) {
-            console.log(err);
-       } else {
-            Project.find({}, function(err, projects) {
-                 if(err) {
-                      console.log(err)
-                 } else {
-                      res.render("post-project", {projects: projects, tools: tools});
-                 }  
-            }); 
-       }
-  });
-});
+router.get('/post-project', function (req, res) {
+  Tool.find({}, function (err, tools) {
+    if (err) {
+      console.log(err)
+    } else {
+      Project.find({}, function (err, projects) {
+        if (err) {
+          console.log(err)
+        } else {
+          res.render('post-project', { projects: projects, tools: tools })
+        }
+      })
+    }
+  })
+})
 
 // Add a new project
 
@@ -42,38 +64,39 @@ router.post('/post-project', (req, res, next) => {
 
   console.log('test: ', req.file)
 
-  Project.create({ projectname, description, toolsNeeded  })
-    .then(tool => {
-      console.log('Created Tool: ',tool)
+  Project.create({ projectname, description, toolsNeeded })
+    .then((tool) => {
+      console.log('Created Tool: ', tool)
       res.redirect('/post-project')
     })
-    .catch(err => next(err))
-});
+    .catch((err) => next(err))
+})
 
 // Delete a project from the board
 
 router.get('/post-project/delete/:id', (req, res, next) => {
-	const id = req.params.id
-	Project.findByIdAndDelete(id)
-		.then(deletedProject => {
-			console.log(deletedProject)
-			res.redirect('/post-project')
-		})
-		.catch(err => next(err))
-});
+  const id = req.params.id
+  Project.findByIdAndDelete(id)
+    .then((deletedProject) => {
+      console.log(deletedProject)
+      res.redirect('/post-project')
+    })
+    .catch((err) => next(err))
+})
 
 // view details of the project
 
 router.get('/post-project/:id', (req, res, next) => {
-	const id = req.params.id
-	Project.findById(id).populate('toolsNeeded')
-		.then(projectsFromDB => {
-      console.log('id: ',id)
-			console.log(projectsFromDB)
-			res.render('project-details', {projects: projectsFromDB})
-		})
-		.catch(err => next(err))
-});
+  const id = req.params.id
+  Project.findById(id)
+    .populate('toolsNeeded')
+    .then((projectsFromDB) => {
+      console.log('id: ', id)
+      console.log(projectsFromDB)
+      res.render('project-details', { projects: projectsFromDB })
+    })
+    .catch((err) => next(err))
+})
 
 // edit the project
 
@@ -82,35 +105,63 @@ router.get('/post-project/edit/:id', (req, res, next) => {
   const projects = Project.findById(id).populate('toolsNeeded')
   const tools = Tool.find()
 
-  Promise.all([projects, tools]).then(data => {
-    const [projects, tools] = data;
-    res.render('project-edit', { projects, tools })
-  })
-  .catch(err => next(err))		
-});
+  Promise.all([projects, tools])
+    .then((data) => {
+      const [projects, tools] = data
+      res.render('project-edit', { projects, tools })
+    })
+    .catch((err) => next(err))
+})
 
 router.post('/post-project/update/:id', (req, res, next) => {
-	const { projectname, description, toolsNeeded } = req.body
-	// by passing {new true} as a third param findByIdAndUpdate returns 
-	
-	Project.findByIdAndUpdate(req.params.id, {
-		projectname,
-		description,
-		toolsNeeded
+  const { projectname, description, toolsNeeded } = req.body
+  // by passing {new true} as a third param findByIdAndUpdate returns
 
-	}, { new: true })
-		.then(updatedPtoject => {
-			console.log(updatedPtoject)
-			res.redirect(`/post-project`)
-		})
-});
+  Project.findByIdAndUpdate(
+    req.params.id,
+    {
+      projectname,
+      description,
+      toolsNeeded,
+    },
+    { new: true }
+  ).then((updatedPtoject) => {
+    console.log(updatedPtoject)
+    res.redirect(`/post-project`)
+  })
+})
 
-router.get("/tool-to-offer", (req, res, next) => {
-  res.render("tool-to-offer");
-});
+router.get('/tool-to-offer', (req, res, next) => {
+  res.render('tool-to-offer')
+})
 
-router.get("/matches", (req, res, next) => {
-  res.render("matches");
-});
+// Select tools from the matches page
 
-module.exports = router;
+router.get('/matches', (req, res, next) => {
+  Tool.find().then((allTools) => {
+    res.render('matches', { allTools })
+  })
+})
+
+// match the tools selected to a project
+router.get('/match', (req, res, next) => {
+  const tool = req.query.match
+  console.log(tool)
+  Tool.find({ name: tool })
+
+    .then((matches) => {
+      console.log(matches)
+      matches.forEach((match) => {
+        let id = mongoose.Types.ObjectId(match._id)
+        Project.find({ toolsNeeded: { $in: [id] } }).then((foundMatches) => {
+          console.log(`this is the found: ${foundMatches}`)
+          res.render('match', { foundMatches })
+        })
+      })
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
+
+module.exports = router
